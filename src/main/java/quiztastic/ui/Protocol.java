82f1f6388db38433c321895120465a1be9dcd450
Christian Gram Kalhauge <christian@kalhauge.dk>
2020-09-07 16:04:48 +0200

    Add examples to Protocol.java

--- src/main/java/quiztastic/ui/Protocol.java
+++ src/main/java/quiztastic/ui/Protocol.java
@@ -1,10 +1,12 @@
 package quiztastic.ui;
 
 import quiztastic.app.Quiztastic;
+import quiztastic.domain.Game;
 
 import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.List;
 import java.util.Scanner;
 
 public class Protocol {
@@ -21,25 +23,59 @@ public class Protocol {
     private String fetchCommand () {
         out.print("> ");
         out.flush();
-        String line = in.nextLine().strip();
-        return line;
+        String word = in.next(); // answer a100 -> answer
+        return word;
     }
 
     public void run () {
-        String line = fetchCommand();
-        while (!line.equals("quit")) {
-            switch (line) {
+        String cmd = fetchCommand();
+        while (!cmd.equals("quit")) {
+            switch (cmd) {
                 case "h":
                 case "help":
                    out.println("There are no help!");
                    break;
+                case "draw":
+                case "d":
+                    displayBoard();
+                    break;
+                case "answer":
+                case "a":
+                    String question = in.next();
+                    String a = question.substring(0, 1).toLowerCase(); // "A100" -> "a"
+                    int questionScore = Integer.parseInt(question.substring(1)); // "A100" -> 100
+                    answerQuestion("abcdef".indexOf(a), questionScore);
+                    break;
                 default:
-                   out.println("Unknown command! " + line);
+                   out.println("Unknown command! " + cmd);
             }
+            in.nextLine();
             out.flush();
-            line = fetchCommand();
+            cmd = fetchCommand();
         }
 
+    }
+
+    private void answerQuestion(int categoryNumber, int questionScore) {
+
+    }
+
+    private void displayBoard() {
+        Game game = quiz.getCurrentGame();
+        List<Integer> scores = List.of(100,200,300,400,500);
+        for (int questionNumber = 0; questionNumber < 5; questionNumber++) {
+            out.print("|");
+            for (int category = 0; category < 6; category++) {
+                out.print("    ");
+                if (game.isAnswered(category, questionNumber)) {
+                    out.print("---");
+                } else {
+                    out.print(scores.get(questionNumber));
+                }
+                out.print("    |");
+            }
+            out.println();
+        }
     }
 
 }
